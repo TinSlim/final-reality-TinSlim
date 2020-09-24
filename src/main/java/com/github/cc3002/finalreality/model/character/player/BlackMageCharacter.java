960e0603a2b0bc94b337b7a5b53055eed1c0b8b7
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-09-23 23:42:11 -0300

    🧙‍♂️Clase mago oscuro con métodos para las magias.

---
+++ src/main/java/com/github/cc3002/finalreality/model/character/player/BlackMageCharacter.java
@@ -0,0 +1,29 @@
+package com.github.cc3002.finalreality.model.character.player;
+
+import com.github.cc3002.finalreality.model.character.Enemy;
+import com.github.cc3002.finalreality.model.character.ICharacter;
+import com.github.cc3002.finalreality.model.weapon.Weapon;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.BlockingQueue;
+
+public class BlackMageCharacter extends PlayerCharacter{
+    /**
+     * Creates a new character.
+     *
+     * @param name           the character's name
+     * @param turnsQueue     the queue with the characters waiting for their turn
+     * @param characterClass
+     */
+    public BlackMageCharacter(@NotNull String name, @NotNull BlockingQueue<ICharacter> turnsQueue, CharacterClass characterClass) {
+        super(name, turnsQueue, characterClass);
+    }
+
+    public void thunder(Enemy target) {
+
+    }
+
+    public void fire(Enemy target) {
+
+    }
+}
