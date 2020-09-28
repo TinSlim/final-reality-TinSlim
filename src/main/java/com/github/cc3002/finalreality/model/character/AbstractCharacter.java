dda9f77bdaf039c1f94d9f01375937855b9155cb
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-09-27 21:38:51 -0300

    The class now has a constructor that can receive maxHp and defense.

--- src/main/java/com/github/cc3002/finalreality/model/character/AbstractCharacter.java
+++ src/main/java/com/github/cc3002/finalreality/model/character/AbstractCharacter.java
@@ -1,12 +1,10 @@
 package com.github.cc3002.finalreality.model.character;
 
 import com.github.cc3002.finalreality.model.character.player.CharacterClass;
-import com.github.cc3002.finalreality.model.character.player.PlayerCharacter;
-import com.github.cc3002.finalreality.model.weapon.Weapon;
+
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -20,46 +18,46 @@ public abstract class AbstractCharacter implements ICharacter {
   protected final BlockingQueue<ICharacter> turnsQueue;
   protected final String name;
   private final CharacterClass characterClass;
-  private Weapon equippedWeapon = null;
-  private ScheduledExecutorService scheduledExecutor;
-
-  /**   Added
-            **/
+  protected ScheduledExecutorService scheduledExecutor;
   private int maxHp;
   private int hp;
+  private int defense;
 
 
   protected AbstractCharacter(@NotNull BlockingQueue<ICharacter> turnsQueue,
-      @NotNull String name, CharacterClass characterClass) {
+      @NotNull String name, CharacterClass characterClass, int maxHp, int defense) {
+    this.turnsQueue = turnsQueue;
+    this.name = name;
+    this.characterClass = characterClass;
+    this.maxHp = maxHp;
+    this.hp = maxHp;
+    this.defense = defense;
+
+  }
+
+  //Todo borrar despues
+  protected AbstractCharacter(@NotNull BlockingQueue<ICharacter> turnsQueue,
+                              @NotNull String name, CharacterClass characterClass) {
     this.turnsQueue = turnsQueue;
     this.name = name;
     this.characterClass = characterClass;
 
-    /*    Added     */
     this.maxHp = 100;
-    this.hp = this.maxHp;
+    this.hp = maxHp;
+    this.defense = 5;
+
   }
 
-  @Override
-  public void waitTurn() {
-    scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-    if (this instanceof PlayerCharacter) {
-      scheduledExecutor
-          .schedule(this::addToQueue, equippedWeapon.getWeight() / 10, TimeUnit.SECONDS);
-    } else {
-      var enemy = (Enemy) this;
-      scheduledExecutor
-          .schedule(this::addToQueue, enemy.getWeight() / 10, TimeUnit.SECONDS);
-    }
-  }
+
 
   /**
    * Adds this character to the turns queue.
    */
-  private void addToQueue() {
+  protected void addToQueue() {
     turnsQueue.add(this);
     scheduledExecutor.shutdown();
   }
+
 
   @Override
   public String getName() {
@@ -67,49 +65,35 @@ public abstract class AbstractCharacter implements ICharacter {
   }
 
   @Override
-  public void equip(Weapon weapon) {
-    if (this instanceof PlayerCharacter) {
-      this.equippedWeapon = weapon;
-    }
-  }
-
-  @Override
-  public Weapon getEquippedWeapon() {
-    return equippedWeapon;
-  }
-
-  @Override
   public CharacterClass getCharacterClass() {
     return characterClass;
   }
 
-  /*added*/
-
+  @Override
   public int getHp() {
-      return this.hp;
+    return this.hp;
   }
+
+  @Override
+  public int getMaxHp() {
+    return this.maxHp;
+  }
+
 
   public void setHp(int number) {
-      this.hp = number;
+    this.hp = number;
   }
 
-  public int getMaxHp() {
-      return this.maxHp;
+  public void setMaxHp(int number) {
+    this.maxHp = number;
   }
+
   public void receiveDamage(int damage) {
-      int actualHp = this.getHp();
-      if (actualHp - damage <= 0) {
-          this.setHp(0);
-      } else {
-          this.setHp(actualHp - damage);
-
-      }
-  }
-
-
-  public void commonAttack(AbstractCharacter target) {
-      Weapon weapon = this.getEquippedWeapon();
-      int damage = weapon.getDamage();
-      target.receiveDamage(damage);
+    int actualHp = this.getHp();
+    if (actualHp - damage <= 0) {
+      this.setHp(0);
+    } else {
+      this.setHp(actualHp - damage);
+    }
   }
 }
