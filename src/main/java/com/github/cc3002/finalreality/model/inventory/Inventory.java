2b8ca0ab12e69fb61637a58e9f020d0aab7d1d5c
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-09-27 21:04:07 -0300

    First idea for an inventory

---
+++ src/main/java/com/github/cc3002/finalreality/model/inventory/Inventory.java
@@ -0,0 +1,37 @@
+package com.github.cc3002.finalreality.model.inventory;
+
+import com.github.cc3002.finalreality.model.character.ICharacter;
+import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
+import com.github.cc3002.finalreality.model.weapon.AbstractWeapon;
+import com.github.cc3002.finalreality.model.weapon.IWeapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<AbstractWeapon> weaponsInventory = new ArrayList<AbstractWeapon>();
+    int pointInventory = 0;
+    int lenInventory = 0;
+
+    public Inventory() {  // Todo Ver el constructor
+         }
+
+    public void addToInventory(AbstractWeapon weapon){
+        this.weaponsInventory.add(weapon);
+        this.lenInventory += 1;
+    }
+
+    public void movePoint(int slots) {
+        if (this.lenInventory - 1 <= this.pointInventory + slots) {
+            this.pointInventory = this.pointInventory + slots;
+        } else {
+            this.pointInventory = this.lenInventory - 1;
+        }
+    }
+
+    public void equipWeaponToCharacter (IPlayerCharacter character) {
+        character.equip(this.weaponsInventory.get(this.pointInventory));
+    }
+
+
+}
