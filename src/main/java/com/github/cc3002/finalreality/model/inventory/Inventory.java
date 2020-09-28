63c3cce12c7133de1e0a1ea72e826978eb0098d6
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-09-27 21:37:00 -0300

    The list uses IWeapon insted of AbstractWeapon

--- src/main/java/com/github/cc3002/finalreality/model/inventory/Inventory.java
+++ src/main/java/com/github/cc3002/finalreality/model/inventory/Inventory.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inventory {
-    private List<AbstractWeapon> weaponsInventory = new ArrayList<AbstractWeapon>();
+    private List<IWeapon> weaponsInventory = new ArrayList<IWeapon>();
     int pointInventory = 0;
     int lenInventory = 0;
 
