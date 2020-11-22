642f9c03564e8b9bd879806eb6d03ece7f1ccdb0
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-22 04:03:02 -0300

    A lot of changes, now List instead of HashMaps, added geter and inventory use.

--- src/main/java/com/github/cc3002/finalreality/controller/Controller.java
+++ src/main/java/com/github/cc3002/finalreality/controller/Controller.java
@@ -1,73 +1,183 @@
 package com.github.cc3002.finalreality.controller;
 
+import com.github.cc3002.finalreality.controller.characterfactory.CharacterMaker;
+import com.github.cc3002.finalreality.controller.weaponfactory.WeaponMaker;
 import com.github.cc3002.finalreality.model.character.Enemy;
 import com.github.cc3002.finalreality.model.character.ICharacter;
 import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
-import com.github.cc3002.finalreality.model.character.player.commoncharacter.EngineerCharacter;
-import com.github.cc3002.finalreality.model.character.player.commoncharacter.KnightCharacter;
-import com.github.cc3002.finalreality.model.character.player.commoncharacter.ThiefCharacter;
-import com.github.cc3002.finalreality.model.character.player.magecharacter.BlackMageCharacter;
-import com.github.cc3002.finalreality.model.character.player.magecharacter.WhiteMageCharacter;
 import com.github.cc3002.finalreality.model.inventory.Inventory;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Controller {
-  private List<IPlayerCharacter> playerCharacters;
-  private List<Enemy> enemyCharacters;
-  private List<ICharacter> allCharacters;
-  BlockingQueue<ICharacter> turnsQueue;
+  int inventoryLenght = 7;
 
-  private int quantityPlayerCharacters;
-  private int quantityEnemyCharacters;
+  CharacterMaker characterFactory;
+  WeaponMaker weaponFactory;
 
-  private Inventory weaponsInventory;
+  ArrayList<IPlayerCharacter> alivePlayerCharacters;
 
+  HashMap<String, IPlayerCharacter> faintedPlayerCharacters;
+  ArrayList<Enemy> enemyCharacters;
+
+  protected BlockingQueue<ICharacter> turnsQueue;
+  int playersAlive;
+  int enemiesAlive;
+  PlayerCharacterListener faintPlayerCharactersListener;
+  EnemyListener faintEnemyListener;
+  Inventory inventory;
 
   public Controller () {
-    BlockingQueue<ICharacter> turnsQueue = new LinkedBlockingQueue<>();
-    quantityPlayerCharacters = 0;
-    quantityEnemyCharacters = 0;
-    weaponsInventory = new Inventory();
+    alivePlayerCharacters = new ArrayList<IPlayerCharacter>();
+    faintedPlayerCharacters = new HashMap<>();
+
+    playersAlive = 0;
+    enemiesAlive = 0;
+
+    faintPlayerCharactersListener = new PlayerCharacterListener(this);
+    faintEnemyListener = new EnemyListener(this);
+
+    inventory = new Inventory();
   }
 
 
-  public void makeBlackMage (String name, int maxHp, int defense, int maxMana) {
-    BlackMageCharacter newBlackMage = new BlackMageCharacter(turnsQueue,name,maxHp,defense,maxMana);
-    playerCharacters.add(newBlackMage);
-    allCharacters.add(newBlackMage);
+  public BlockingQueue<ICharacter> getQueue () {
+    return turnsQueue;
   }
 
-  public void makeWhiteMage (String name, int maxHp, int defense, int maxMana) {
-    WhiteMageCharacter newWhiteMage = new WhiteMageCharacter(turnsQueue,name,maxHp,defense,maxMana);
-    playerCharacters.add(newWhiteMage);
-    allCharacters.add(newWhiteMage);
+  /*
+    1 Crear y asignar objetos
+   */
+
+
+  public void setCharacterMaker(CharacterMaker maker) {
+    characterFactory = maker;
+    maker.setController(this);
   }
 
-  public void makeEngineer (String name, int maxHp, int defense) {
-    EngineerCharacter newEngineer = new EngineerCharacter(turnsQueue,name,maxHp,defense);
-    playerCharacters.add(newEngineer);
-    allCharacters.add(newEngineer);
+  public void setWeaponMaker(WeaponMaker maker) {
+    weaponFactory = maker;
+    maker.setController(this);
   }
 
-  public void makeThief (String name, int maxHp, int defense) {
-    ThiefCharacter newThief = new ThiefCharacter(turnsQueue,name,maxHp,defense);
-    playerCharacters.add(newThief);
-    allCharacters.add(newThief);
+  void rightCharacterMaker() {
+    characterFactory.changeRight();
   }
 
-  public void makeKnight (String name, int maxHp, int defense) {
-    KnightCharacter newKnight = new KnightCharacter(turnsQueue,name,maxHp,defense);
-    playerCharacters.add(newKnight);
-    allCharacters.add(newKnight);
+  void leftCharacterMaker() {
+    characterFactory.changeLeft();
   }
 
-  public void makeEnemy (String name, int maxHp, int weight, int defense, int damage) {
-    Enemy newEnemy = new Enemy(turnsQueue,name,maxHp,weight,defense,damage);
-    enemyCharacters.add(newEnemy);
-    allCharacters.add(newEnemy);
+  void enemyMaker () {
+    characterFactory.changeToEnemy();
   }
 
+  public void makeCharacter (String name) {
+    characterFactory.makeCharacter(name);
+  }
+
+  /*
+    2 Personajes del Jugador
+   */
+
+  public ArrayList<IPlayerCharacter> getAlivePlayerCharacters() {
+    return alivePlayerCharacters;
+  }
+
+  public void changePlayersQuantity (int i) {
+    playersAlive += i;
+  }
+
+  /*
+    3 Tener Conocimiento de enemigos y datos
+   */
+
+
+  public ArrayList<Enemy> getEnemyCharacters() {
+    return enemyCharacters;
+  }
+
+  public int getEnemiesAlive() {
+    return enemiesAlive;
+  }
+
+  public void changeEnemyQuantity (int i) {
+    enemiesAlive += i;
+  }
+
+
+
+  public HashMap<String, IPlayerCharacter> getFaintedPlayerCharacters() {
+    return faintedPlayerCharacters;
+  }
+
+  public void win() {
+  }
+
+
+  public void loss() {
+  }
+
+
+
+  public void getPlayersCharactersData () {
+    for (IPlayerCharacter character : alivePlayerCharacters) {
+      character.getData();
+    }
+  }
+
+
+
+
+  public int getPlayersAlive () {
+    return playersAlive;
+  }
+
+  /*
+    4 Manejar el inventario
+    5 Equipar un arma a un personaje
+   */
+
+  public Inventory getInventory () {
+    return inventory;
+  }
+
+  public void rightMoveInventory () {
+    inventory.movePoint(1);
+  }
+
+  public void leftMoveInventory () {
+    inventory.movePoint(-1);
+  }
+
+  public void upMoveInventory () {
+    inventory.movePoint(inventoryLenght);
+  }
+
+  public void downMoveInventory () {
+    inventory.movePoint(-inventoryLenght);
+  }
+
+  public void equipWeaponTo (IPlayerCharacter character) {
+    inventory.equipWeaponToCharacter(character);
+  }
+
+  /*
+    6 Personaje ataque a otro
+   */
+
+  public void enemyAttacks (Enemy enemy, IPlayerCharacter target) {
+    enemy.commonAttack(target);
+  }
+
+  public void playerCharacterCommonAttack (IPlayerCharacter player, Enemy target) {
+    player.commonAttack(target);
+  }
+
+  public CharacterMaker getCharacterMaker() {
+    return characterFactory;
+  }
 }
+
