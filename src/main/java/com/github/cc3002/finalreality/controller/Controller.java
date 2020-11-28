e4ff45689a87a8d362b41100226047bfd3b35179
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-27 21:07:15 -0300

    Added documentation.

--- src/main/java/com/github/cc3002/finalreality/controller/Controller.java
+++ src/main/java/com/github/cc3002/finalreality/controller/Controller.java
@@ -45,12 +45,23 @@ public class Controller {
     inventory = new Inventory();
   }
 
-
+  /**
+   * Returns the turnsQueue
+   * @return turnsQueue
+   */
   public BlockingQueue<ICharacter> getQueue () {
     return turnsQueue;
   }
 
 
+
+  /**
+   * Makes a KnightCharacter, adds to it the listeners, adds 1 to the players quantity and adds the new
+   * character to the alivePlayerCharacters list.
+   * @param name Knight's name.
+   * @param maxHp Knight's maxHp.
+   * @param defense Knight's defense.
+   */
   public void makeKnight (String name, int maxHp, int defense) {
     KnightCharacter character = new KnightCharacter(turnsQueue,name,maxHp,defense);
     character.addDeathListener(faintPlayerCharactersListener);
@@ -59,6 +70,13 @@ public class Controller {
     changePlayersQuantity(1);
   }
 
+  /**
+   * Makes a ThiefCharacter, adds to it the listeners, adds 1 to the players quantity and adds the new
+   * character to the alivePlayerCharacters list.
+   * @param name Thief's name.
+   * @param maxHp Thief's maxHp.
+   * @param defense Thief's defense.
+   */
   public void makeThief (String name, int maxHp, int defense) {
     ThiefCharacter character = new ThiefCharacter(turnsQueue,name,maxHp,defense);
     character.addDeathListener(faintPlayerCharactersListener);
@@ -67,6 +85,13 @@ public class Controller {
     changePlayersQuantity(1);
   }
 
+  /**
+   * Makes a EngineerCharacter, adds to it the listeners, adds 1 to the players quantity and adds the new
+   * character to the alivePlayerCharacters list.
+   * @param name Engineer's name.
+   * @param maxHp Enigneer's maxHp.
+   * @param defense Engineer's defense.
+   */
   public void makeEngineer (String name, int maxHp, int defense) {
     EngineerCharacter character = new EngineerCharacter(turnsQueue, name, maxHp, defense);
     character.addDeathListener(faintPlayerCharactersListener);
@@ -75,6 +100,14 @@ public class Controller {
     changePlayersQuantity(1);
   }
 
+  /**
+   * Makes a WhiteMageCharacter, adds to it the listeners, adds 1 to the players quantity and adds the new
+   * character to the alivePlayerCharacters list.
+   * @param name WhiteMage's name.
+   * @param maxHp WhiteMage's maxHp.
+   * @param defense WhiteMage's defense.
+   * @param maxMana WhiteMage's maxMana.
+   */
   public void makeWhiteMage (String name, int maxHp, int defense, int maxMana) {
     WhiteMageCharacter character = new WhiteMageCharacter(turnsQueue,name,maxHp,defense,maxMana);
     character.addDeathListener(faintPlayerCharactersListener);
@@ -83,6 +116,14 @@ public class Controller {
     changePlayersQuantity(1);
   }
 
+  /**
+   * Makes a BlackMageCharacter, adds to it the listeners, adds 1 to the players quantity and adds the new
+   * character to the alivePlayerCharacters list.
+   * @param name BlackMage's name.
+   * @param maxHp BlackMage's maxHp.
+   * @param defense BlackMage's defense.
+   * @param maxMana BlackMage's maxMana.
+   */
   public void makeBlackMage (String name, int maxHp, int defense, int maxMana) {
     BlackMageCharacter character = new BlackMageCharacter(turnsQueue,name,maxHp,defense,maxMana);
     character.addDeathListener(faintPlayerCharactersListener);
@@ -91,6 +132,15 @@ public class Controller {
     changePlayersQuantity(1);
   }
 
+  /**
+   * Makes a Enemy, adds to it the listeners, adds 1 to the enemy quantity and adds the new character to
+   * enemyCharacters list.
+   * @param name Enemy's name.
+   * @param maxHp Enemy's maxHp.
+   * @param defense Enemy's defense.
+   * @param weight Enemy's weight.
+   * @param damage Enemy's damage.
+   */
   public void makeEnemy (String name, int maxHp, int defense, int weight, int damage) {
     Enemy character = new Enemy(turnsQueue,name,maxHp,weight,defense,damage);
     character.addDeathListener(faintEnemyListener);
@@ -99,30 +149,72 @@ public class Controller {
     changeEnemyQuantity (1);
   }
 
+
+  /**
+   * Makes a Sword and adds it to the inventory.
+   * @param name Sword's name.
+   * @param damage Sword's damage.
+   * @param weight Swords's weight.
+   */
   public void makeSword (String name, int damage, int weight) {
     inventory.addToInventory(new Sword(name,damage,weight));
   }
 
+  /**
+   * Makes an Axe and adds it to the inventory.
+   * @param name Axe's name.
+   * @param damage Axe's damage.
+   * @param weight Axe's weight.
+   */
   public void makeAxe (String name, int damage, int weight) {
     inventory.addToInventory(new Axe(name,damage,weight));
   }
 
+  /**
+   * Makes a Knife and adds it to the inventory.
+   * @param name Knife's name.
+   * @param damage Knife's damage.
+   * @param weight Knife's weight.
+   */
   public void makeKnife (String name, int damage, int weight) {
     inventory.addToInventory(new Knife(name,damage,weight));
   }
 
+  /**
+   * Makes a Bow and adds it to the inventory.
+   * @param name Bow's name.
+   * @param damage Bow's damage.
+   * @param weight Bow's weight.
+   */
   public void makeBow (String name, int damage, int weight) {
     inventory.addToInventory(new Bow(name,damage,weight));
   }
 
+  /**
+   * Makes a Staff and adds it to the inventory.
+   * @param name Staff's name.
+   * @param damage Staff's damage.
+   * @param weight Staff's weight.
+   * @param magicDamage Staff's magicDamage.
+   */
   public void makeStaff (String name, int damage, int weight, int magicDamage) {
     inventory.addToInventory(new Staff(name,damage,magicDamage,weight));
   }
 
+  /**
+   * Returns a player character from the list of playerCharacters, using an index.
+   * @param index index of the character wanted.
+   * @return player character wanted.
+   */
   public IPlayerCharacter getPlayerCharacter (int index) {
     return getAlivePlayerCharacters().get(index);
   }
 
+  /**
+   * Returns an enemy from the list of enemies, using an index.
+   * @param index index of the character wanted.
+   * @return enemy wanted.
+   */
   public Enemy getEnemy (int index) {
     return getEnemyCharacters().get(index);
   }
@@ -191,38 +283,6 @@ public class Controller {
     return enemyCharacters;
   }
 
-  public int getEnemiesAlive() {
-    return enemiesAlive;
-  }
-
-  public void changeEnemyQuantity (int i) {
-    enemiesAlive += i;
-  }
-
-  public void makeAxe (String name) {
-    Axe weapon = new Axe(name,50,60);
-    getInventory().addToInventory(weapon);
-  }
-
-  public void makeSword (String name) {
-    Sword weapon = new Sword(name,50,60);
-    getInventory().addToInventory(weapon);
-  }
-
-  public void makeStaff (String name) {
-    Staff weapon = new Staff(name,50,60,20);
-    getInventory().addToInventory(weapon);
-  }
-
-  public void makeBow (String name) {
-    Bow weapon = new Bow(name,50,60);
-    getInventory().addToInventory(weapon);
-  }
-
-  public void makeKnife (String name) {
-    Knife weapon = new Knife(name,50,60);
-    getInventory().addToInventory(weapon);
-  }
 
   public void win() {
   }
@@ -231,39 +291,88 @@ public class Controller {
   public void loss() {
   }
 
+  /**
+   * Returns the quantity of alive enemies.
+   * @return enemiesAlive.
+   */
+  public int getEnemiesAlive() {
+    return enemiesAlive;
+  }
 
+  /**
+   * Adds i to the alive enemies.
+   * @param i number added to enemiesAlive.
+   */
+  public void changeEnemyQuantity (int i) {
+    enemiesAlive += i;
+  }
+
+  /**
+   * Returns the number of alive PlayerCharacters.
+   * @return number of alive PlayerCharacters.
+   */
   public int getPlayersAlive () {
     return playersAlive;
   }
 
+  /**
+   * Returns the inventory with the weapons.
+   * @return inventory.
+   */
   public Inventory getInventory () {
     return inventory;
   }
 
+  /**
+   * Moves the inventory pointer right.
+   */
   public void rightMoveInventory () {
-    inventory.movePoint(1);
+    getInventory().movePoint(1);
   }
 
+  /**
+   * Moves the inventory pointer left.
+   */
   public void leftMoveInventory () {
-    inventory.movePoint(-1);
+    getInventory().movePoint(-1);
   }
 
+  /**
+   * Moves the inventory pointer up.
+   */
   public void upMoveInventory () {
-    inventory.movePoint(inventoryLenght);
+    getInventory().movePoint(inventoryLenght);
   }
 
+  /**
+   * Moves the inventory pointer down.
+   */
   public void downMoveInventory () {
-    inventory.movePoint(-inventoryLenght);
+    getInventory().movePoint(-inventoryLenght);
   }
 
+  /**
+   * The character equips the weapon pointed in the inventory.
+   * @param character who equips the weapon.
+   */
   public void equipWeaponTo (IPlayerCharacter character) {
-    inventory.equipWeaponToCharacter(character);
+    getInventory().equipWeaponToCharacter(character);
   }
 
+  /**
+   * The enemy Enemy do a commonAttack to the target.
+   * @param enemy who attacks.
+   * @param target who receives the attack.
+   */
   public void enemyAttacks (Enemy enemy, IPlayerCharacter target) {
     enemy.commonAttack(target);
   }
 
+  /**
+   * The player IPlayerCharacter do a commonAttack to the target.
+   * @param player who attacks.
+   * @param target who receives the attack.
+   */
   public void playerCharacterCommonAttack (IPlayerCharacter player, Enemy target) {
     player.commonAttack(target);
   }
