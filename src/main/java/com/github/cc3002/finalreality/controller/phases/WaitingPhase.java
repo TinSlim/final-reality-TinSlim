be1acb51c0f66667a61b7c0df6f9acdc66b56e6f
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-15 22:30:00 -0300

    Added doc.

--- src/main/java/com/github/cc3002/finalreality/controller/phases/WaitingPhase.java
+++ src/main/java/com/github/cc3002/finalreality/controller/phases/WaitingPhase.java
@@ -1,7 +1,11 @@
-package com.github.cc3002.finalreality.controller.turns;
+package com.github.cc3002.finalreality.controller.phases;
 
-public class WaitingPhase extends Phase {
+public class WaitingPhase extends AbstractPhase {
 
+  /**
+   * Start a new turn, changing to decision phase.
+   */
+  @Override
   public void newTurn() {
     changePhase(new DecisionPhase());
   }
