f35dfc32318442747171416e0071e0ba5584200d
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-17 20:30:16 -0300

    A class to do the view of characters.

---
+++ src/main/java/com/github/cc3002/finalreality/gui/BattleView.java
@@ -0,0 +1,94 @@
+package com.github.cc3002.finalreality.gui;
+
+import com.github.cc3002.finalreality.controller.Controller;
+import com.github.cc3002.finalreality.model.character.Enemy;
+import com.github.cc3002.finalreality.model.character.ICharacter;
+import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
+import com.github.cc3002.finalreality.model.weapon.IWeapon;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleView {
+  Group actualPlayers;
+  ArrayList<Label> hpVisible;
+  Group actualEnemies;
+  Group actualWeapons;
+
+  Controller controller;
+
+  public void setController (Controller controller) {
+    this.controller = controller;
+  }
+
+  public void setPlayerValues () {
+    int i = 20;
+    int j = 10;
+    Group players = new Group();
+    for (IPlayerCharacter player : controller.getPlayerCharacters()) {
+      Group actualPlayer = new Group();
+      Label name = new Label(controller.getPlayerCharacterName(player));
+      Label hp = new Label("HP:"+controller.getPlayerCharacterMaxHp(player)+"/"+controller.getPlayerCharacterHp(player));
+      hpVisible.add(hp);
+      Label defense = new Label("Def:"+controller.getPlayerCharacterDefense(player));
+
+      Image image = new Image(controller.getImage((ICharacter) player),160,60,false,
+              true);
+      ImageView imageView = new ImageView(image);
+
+      actualPlayer.getChildren().add(imageView);
+      actualPlayer.getChildren().add(name);
+      actualPlayer.getChildren().add(hp);
+      name.setLayoutY(0);
+      hp.setLayoutY(0+12);
+      defense.setLayoutY(0+24);
+      imageView.setLayoutX(5);
+      actualPlayer.setLayoutY(i);
+      i += 70;
+
+
+      actualPlayer.getChildren().add(defense);
+      players.getChildren().add(actualPlayer);
+    }
+    actualPlayers = players;
+  }
+
+  public void updateHp () {
+    for (int i = 0; i < controller.getPlayerCharacters().size(); i++) {
+      hpVisible.get(i).setText("HP:"+controller.getPlayerCharacterMaxHp(controller.getPlayerCharacter(i))+"/"+
+              controller.getPlayerCharacter(i));
+    }
+  }
+
+  public Group getActualPlayers () {
+    return actualPlayers;
+  }
+
+  public void setEquipment () {
+    Group weapons = new Group();
+    for (IWeapon weapon : controller.getInventory().getWeaponsInventory()) {
+      Group actualWeapon = new Group();
+      Label name = new Label(weapon.getName());
+      Label damage = new Label("Damage:"+weapon.getDamage());
+
+      Image image = new Image(weapon.getImage(),160,60,false,
+              true);
+      ImageView imageView = new ImageView(image);
+
+      actualWeapon.getChildren().add(imageView);
+      actualWeapon.getChildren().add(name);
+      actualWeapon.getChildren().add(damage);
+      weapons.getChildren().add(actualWeapon);
+    }
+    actualWeapons = weapons;
+  }
+
+  public Group getActualWeapons () {
+    return actualWeapons;
+  }
+}
