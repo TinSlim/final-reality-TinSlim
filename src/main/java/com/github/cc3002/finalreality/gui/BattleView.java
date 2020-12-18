9d048af4f025a512cf27cc903f8669980f35e63a
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-18 02:05:02 -0300

    All characters appears, added pointers and weapons too.

--- src/main/java/com/github/cc3002/finalreality/gui/BattleView.java
+++ src/main/java/com/github/cc3002/finalreality/gui/BattleView.java
@@ -6,25 +6,44 @@ import com.github.cc3002.finalreality.model.character.ICharacter;
 import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
 import com.github.cc3002.finalreality.model.weapon.IWeapon;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 public class BattleView {
   Group actualPlayers;
-  ArrayList<Label> hpVisible;
+  ArrayList<Label> playerHpVisible;
+  ArrayList<Label> enemyHpVisible;
   Group actualEnemies;
   Group actualWeapons;
-
   Controller controller;
+  ImageView targetPointer;
+  ImageView equipmentPointer;
 
   public BattleView () {
-    hpVisible = new ArrayList<>();
+    playerHpVisible = new ArrayList<>();
+    enemyHpVisible = new ArrayList<>();
+  }
+
+  public void initializeTargetPointer () {
+    File file = new File("src\\resources\\targetEnemy.png");
+    String imageUrl = file.toURI().toString();
+    Image image = new Image(imageUrl,160,60,false,true);
+    targetPointer = new ImageView(image);
+    targetPointer.setLayoutX(200);
+    targetPointer.setLayoutY(130);
+  }
+
+  public void initializeEquipmentPointer () {
+    File file = new File("src\\resources\\targetWeapon.png");
+    String imageUrl = file.toURI().toString();
+    Image image = new Image(imageUrl,150,70,false,true);
+    equipmentPointer = new ImageView(image);
+    equipmentPointer.setLayoutX(200);
+    equipmentPointer.setLayoutY(130);
   }
 
   public void setController (Controller controller) {
@@ -39,7 +58,7 @@ public class BattleView {
       Group actualPlayer = new Group();
       Label name = new Label(controller.getPlayerCharacterName(player));
       Label hp = new Label("HP:"+controller.getPlayerCharacterMaxHp(player)+"/"+controller.getPlayerCharacterHp(player));
-      hpVisible.add(hp);
+      playerHpVisible.add(hp);
       Label defense = new Label("Def:"+controller.getPlayerCharacterDefense(player));
 
       File file = new File(controller.getImage((ICharacter) player));
@@ -53,8 +72,8 @@ public class BattleView {
       actualPlayer.getChildren().add(name);
       actualPlayer.getChildren().add(hp);
       name.setLayoutY(0);
-      hp.setLayoutY(0+12);
-      defense.setLayoutY(0+24);
+      hp.setLayoutY(12);
+      defense.setLayoutY(24);
       imageView.setLayoutX(5);
       actualPlayer.setLayoutY(i);
       i += 70;
@@ -66,10 +85,55 @@ public class BattleView {
     actualPlayers = players;
   }
 
+  public void setEnemyValues () {
+    int i = 20;
+    int j = 200;
+    Group enemies = new Group();
+    for (Enemy enemy : controller.getEnemyCharacters()) {
+      Group actualEnemy = new Group();
+      Label name = new Label(controller.getEnemyName(enemy));
+      Label hp = new Label("HP:"+controller.getEnemyMaxHp(enemy)+"/"+controller.getEnemyHp(enemy));
+      enemyHpVisible.add(hp);
+      Label defense = new Label("Def:"+controller.getEnemyDefense(enemy));
+      Label damage = new Label("Damage:"+controller.getEnemyDamage(enemy));
+
+      File file = new File(controller.getImage(enemy));
+      String imageUrl = file.toURI().toString();
+
+      Image image = new Image(imageUrl,160,60,false,
+              true);
+      ImageView imageView = new ImageView(image);
+
+      actualEnemy.getChildren().add(imageView);
+      actualEnemy.getChildren().add(name);
+      actualEnemy.getChildren().add(hp);
+      actualEnemy.getChildren().add(damage);
+      name.setLayoutY(0);
+      hp.setLayoutY(12);
+      defense.setLayoutY(24);
+      damage.setLayoutY(36);
+      imageView.setLayoutX(5);
+      actualEnemy.setLayoutY(i);
+      i += 70;
+      actualEnemy.setLayoutX(j);
+      actualEnemy.getChildren().add(defense);
+      enemies.getChildren().add(actualEnemy);
+    }
+    actualEnemies = enemies;
+  }
+
+  public Group getActualEnemies () {
+    return actualEnemies;
+  }
+
   public void updateHp () {
     for (int i = 0; i < controller.getPlayerCharacters().size(); i++) {
-      hpVisible.get(i).setText("HP:"+controller.getPlayerCharacterMaxHp(controller.getPlayerCharacter(i))+"/"+
-              controller.getPlayerCharacter(i));
+      playerHpVisible.get(i).setText("HP:"+controller.getPlayerCharacterMaxHp(controller.getPlayerCharacter(i))+"/"+
+              controller.getPlayerCharacterHp(controller.getPlayerCharacter(i)));
+    }
+    for (int i = 0; i < controller.getEnemyCharacters().size(); i++) {
+      enemyHpVisible.get(i).setText("HP:"+controller.getEnemyMaxHp(controller.getEnemy(i))+"/"+
+              controller.getEnemyHp(controller.getEnemy(i)));
     }
   }
 
@@ -117,5 +181,15 @@ public class BattleView {
 
   public Group getActualWeapons () {
     return actualWeapons;
+  }
+
+  public void updateAttackPointer() {
+    targetPointer.setLayoutY(30 + 70 * controller.getAttackPointer());
+  }
+
+  public void updateEquipmentPointer() {
+    int value = controller.getEquipmentPointer();
+    equipmentPointer.setLayoutY(320 + (value / 4) * 70);
+    equipmentPointer.setLayoutX(5 + (value % 4) * 165);
   }
 }
