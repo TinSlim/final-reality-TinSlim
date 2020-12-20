a918b15ecabe25600b72377581b25d70670f4ef7
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-19 22:20:46 -0300

    Common methods and attributes for all views.

--- src/main/java/com/github/cc3002/finalreality/gui/View.java
+++ src/main/java/com/github/cc3002/finalreality/gui/View.java
@@ -1,4 +1,33 @@
 package com.github.cc3002.finalreality.gui;
 
-public class View {
+import com.github.cc3002.finalreality.controller.Controller;
+import javafx.scene.Group;
+
+public abstract class View implements IView {
+  Group root;
+  Controller controller;
+  FinalReality aGame;
+
+  public void setRoot(Group newRoot) {
+    root = newRoot;
+  }
+
+  @Override
+  public void setController(Controller newController) {
+    controller = newController;
+  }
+
+  @Override
+  public void setGame(FinalReality game) {
+    aGame = game;
+  }
+
+  public abstract void initialize ();
+
+  public abstract void update ();
+
+  @Override
+  public Group getRoot() {
+    return root;
+  }
 }
