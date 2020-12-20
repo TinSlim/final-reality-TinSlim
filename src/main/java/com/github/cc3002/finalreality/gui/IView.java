326f2c900330a117fa0b4202e65eb9316227d07a
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-19 22:20:21 -0300

    Interface for all views.

--- src/main/java/com/github/cc3002/finalreality/gui/IView.java
+++ src/main/java/com/github/cc3002/finalreality/gui/IView.java
@@ -1,4 +1,18 @@
 package com.github.cc3002.finalreality.gui;
 
+import com.github.cc3002.finalreality.controller.Controller;
+import javafx.scene.Group;
+
 public interface IView {
+  void setRoot(Group grupo);
+
+  void setController(Controller controller);
+
+  void setGame(FinalReality finalReality);
+
+  void initialize();
+
+  void update();
+
+  Group getRoot();
 }
