2da72c2642f1ea9ad23d4b8bdf32efdac91dbeb1
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-21 00:47:08 -0300

    Added and fixed tests.

--- src/test/java/com/github/cc3002/finalreality/controller/ControllerTest.java
+++ src/test/java/com/github/cc3002/finalreality/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package com.github.cc3002.finalreality.controller;
 
+import com.github.cc3002.finalreality.controller.phases.PlayerPhase;
 import com.github.cc3002.finalreality.model.character.Enemy;
 import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
 import com.github.cc3002.finalreality.model.character.player.commoncharacter.EngineerCharacter;
@@ -36,25 +37,32 @@ public class ControllerTest {
     }
     KnightCharacter testKnight1 = new KnightCharacter(testController.getQueue(),
             "TestKnight1",1,1);
+    testKnight1.equipWeapon(new Sword("DummySword",9,9));
 
+    testController.setPhase(new PlayerPhase());
+
+    assertEquals(0,testController.getEquipmentPointer());
     testController.rightMoveInventory();
+    assertEquals(1,testController.getEquipmentPointer());
     testController.rightMoveInventory();
+    assertEquals(2,testController.getEquipmentPointer());
     testController.equipWeaponTo(testKnight1);
-    assertEquals(new Sword("TestSword1",50,60) ,testKnight1.getEquippedWeapon());
+    assertEquals(new Sword("TestSword2",50,60) ,testKnight1.getEquippedWeapon());
 
-    testController.upMoveInventory();
+    testController.downMoveInventory();
+    assertEquals(6,testController.getEquipmentPointer());
     testController.equipWeaponTo(testKnight1);
     assertEquals(new Sword("TestSword6",50,60) ,testKnight1.getEquippedWeapon());
 
-    testController.downMoveInventory();
+    testController.upMoveInventory();
+    assertEquals(2,testController.getEquipmentPointer());
     testController.equipWeaponTo(testKnight1);
-    assertEquals(new Sword("TestSword1",50,60) ,testKnight1.getEquippedWeapon());
+    assertEquals(new Sword("TestSword2",50,60) ,testKnight1.getEquippedWeapon());
 
     testController.leftMoveInventory();
+    assertEquals(1,testController.getEquipmentPointer());
     testController.equipWeaponTo(testKnight1);
-    assertEquals(new Sword("TestSword0",50,60) ,testKnight1.getEquippedWeapon());
-
-
+    assertEquals(new Sword("TestSword1",50,60) ,testKnight1.getEquippedWeapon());
   }
 
   /**
@@ -102,9 +110,6 @@ public class ControllerTest {
     assertEquals(new BlackMageCharacter(testController.getQueue(),"TestBlackMage",4,4,4),
             testController.getPlayerCharacter(3));
 
-    assertEquals(new WhiteMageCharacter(testController.getQueue(),"TestWhiteMage",5,5,5),
-            testController.getPlayerCharacter(4));
-
     assertEquals(new Enemy(testController.getQueue(),"TestEnemy1",6,6,6,6),
             testController.getEnemy(0));
 
@@ -125,73 +130,80 @@ public class ControllerTest {
     testController.makeStaff("TestStaff",7,7,7);
 
     assertEquals(new Sword("TestSword",3,3),
-            testController.getInventory().getWeaponsInventory().get(1));
+            testController.getInventory().getWeaponsInventory().get(0));
 
     assertEquals(new Bow("TestBow",4,4),
-            testController.getInventory().getWeaponsInventory().get(2));
+            testController.getInventory().getWeaponsInventory().get(1));
 
     assertEquals(new Knife("TestKnife",5,5),
-            testController.getInventory().getWeaponsInventory().get(3));
+            testController.getInventory().getWeaponsInventory().get(2));
 
     assertEquals(new Axe("TestAxe",6,6),
-            testController.getInventory().getWeaponsInventory().get(4));
+            testController.getInventory().getWeaponsInventory().get(3));
 
     assertEquals(new Staff("TestStaff",7,7,7),
-            testController.getInventory().getWeaponsInventory().get(5));
+            testController.getInventory().getWeaponsInventory().get(4));
   }
 
   /**
    * Tests common attacks and weapon equip.
    */
   @Test
-  public void testAttackAndEquip () throws InterruptedException {
-    testController.makeSword("TestSword",9,40);
-    testController.makeAxe("TestAxe",2,60);
-    testController.makeBow("TestBow",4,120);
-    testController.makeKnife("TestKnife",10,100);
-    testController.makeStaff("TestStaff",15,160,7);
-
+  public void testAttack() throws InterruptedException {
     testController.makeKnight("TestKnight",50,0);
     testController.makeEngineer("TestEngineer",50,0);
     testController.makeThief("TestThief",50,0);
     testController.makeBlackMage("TestBlackMage",50,0,4);
-    testController.makeWhiteMage("TestWhiteMage",50,0,5);
 
     testController.makeEnemy("TestEnemy1",10,0,140,3);
     testController.makeEnemy("TestEnemy2",2,0,80,4);
     testController.makeEnemy("TestEnemy3",10,0,20,50);
 
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(0));
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(1));
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(2));
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(3));
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(4));
+    testController.getPlayerCharacter(0).equip(new Sword("TestSword",9,40));
+    testController.getPlayerCharacter(1).equip(new Axe("TestAxe",2,60));
+    testController.getPlayerCharacter(2).equip(new Bow("TestBow",4,120));
+    testController.getPlayerCharacter(3).equip(new Knife("TestKnife",10,100));
+
+    boolean outOfBoundCharacterList = false;
+    try {
+      testController.getPlayerCharacter(4).equip(new Staff("TestStaff",15,160,7));
+    } catch (IndexOutOfBoundsException e) {
+      outOfBoundCharacterList = true;
+    }
+    assertTrue(outOfBoundCharacterList);
 
     testController.startGame();
 
     Thread.sleep(2500);
     testController.executePhase();
 
+
     Thread.sleep(2500);
     testController.executePhase();
+
+    Thread.sleep(2500);
+    testController.executePhase();
+    assertEquals(0,testController.getPlayerAttackingPointer());
+    assertEquals(0,testController.getAttackPointer());
     testController.getPhase().moveTargetRight();
+    assertEquals(1,testController.getAttackPointer());
     testController.getPhase().moveTargetRight();
+    assertEquals(2,testController.getAttackPointer());
     testController.getPhase().moveTargetRight();
+    assertEquals(2,testController.getAttackPointer());
     testController.getPhase().moveTargetRight();
     testController.getPhase().moveTargetLeft();
+    assertEquals(1,testController.getAttackPointer());
     testController.getPhase().doAttack();
 
     Thread.sleep(2500);
     testController.executePhase();
+    assertEquals(1,testController.getPlayerAttackingPointer());
     testController.getPhase().doAttack();
 
     Thread.sleep(2000);
     testController.executePhase();
+    assertEquals(0,testController.getPlayerAttackingPointer());
     testController.moveTargetRight();
     testController.moveTargetRight();
     testController.moveTargetRight();
@@ -203,9 +215,20 @@ public class ControllerTest {
 
     Thread.sleep(2000);
     testController.executePhase();
+
+    Thread.sleep(2000);
+    testController.executePhase();
+    testController.executePhase();
+    assertEquals(0,testController.getPlayerAttackingPointer());
+    assertEquals(0,testController.getAttackPointer());
     testController.moveTargetLeft();
+    assertEquals(0,testController.getAttackPointer());
     testController.moveTargetLeft();
+    assertEquals(0,testController.getAttackPointer());
     testController.moveTargetLeft();
+    testController.getPhase().doAttack();
+
+    testController.moveTargetRight();
     testController.getPhase().doAttack();
 
     Thread.sleep(2000);
@@ -286,14 +309,22 @@ public class ControllerTest {
    */
   @Test
   public void testLoseGame () throws InterruptedException {
-    testController.makeSword("TestSword",9,300);
     testController.makeKnight("TestKnight",20,3);
 
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(0));
+    testController.getPlayerCharacter(0).equip(new Sword("TestSword",9,300));
 
     testController.makeEnemy("TestEnemy1",10,0,30,100);
-    testController.makeEnemy("TestEnemy2",10,0,70,100);
+    testController.makeEnemy("TestEnemy2",10,0,50,100);
+    testController.makeEnemy("TestEnemy3",10,0,70,100);
+    testController.makeEnemy("TestEnemy4",10,0,90,100);
+    testController.makeEnemy("TestEnemy5",10,0,110,100);
+    testController.makeEnemy("TestEnemy6",10,0,130,100);
+    testController.makeEnemy("TestEnemy7",10,0,150,100);
+    testController.makeEnemy("TestEnemy8",10,0,180,100);
+    testController.makeEnemy("TestEnemy9",10,0,200,100);
+
+    assertTrue(testController.getEnemiesQuantity() < 9);
+
     testController.startGame();
 
     Thread.sleep(2000);
@@ -312,17 +343,15 @@ public class ControllerTest {
    */
   @Test
   public void testWinGame () throws InterruptedException {
-    testController.makeSword("TestSword",100,10);
     testController.makeKnight("TestKnight",20,3);
 
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(0));
+    testController.getPlayerCharacter(0).equip(new Sword("TestSword",100,10));
 
-    testController.makeEnemy("TestEnemy1",10,5,300,2);
+    testController.makeEnemy("TestEnemy1",10,5,40,2);
     testController.startGame();
     Thread.sleep(2000);
     testController.executePhase();
-    testController.getPhase().doAttack();
+    testController.doAttack();
   }
 
   /**
@@ -335,35 +364,41 @@ public class ControllerTest {
       testController.makeSword("TestSword"+String.valueOf(i),3,20);
     }
     testController.makeKnight("TestKnight",100,60);
-    testController.makeEnemy("TestEnemy",100,100,40,100);
-
-    testController.rightMoveInventory();
-    testController.equipWeaponTo(testController.getPlayerCharacter(0));
-    assertEquals(new Sword("TestSword0",3,20),
-            testController.getPlayerCharacter(0).getEquippedWeapon());
+    testController.makeEnemy("TestEnemy",100,100,100,100);
 
     testController.startGame();
     Thread.sleep(4000);
     testController.executePhase();
-    testController.getPhase().moveRightInventory();
-    testController.getPhase().moveRightInventory();
-    testController.getPhase().equipWeapon();
-    assertEquals(new Sword("TestSword2",3,20),
-            testController.getPlayerCharacter(0).getEquippedWeapon());
-
-    testController.getPhase().moveUpInventory();
-    testController.getPhase().equipWeapon();
-    assertEquals(new Sword("TestSword7",3,20),
-            testController.getPlayerCharacter(0).getEquippedWeapon());
-
-    testController.getPhase().moveDownInventory();
-    testController.getPhase().equipWeapon();
-    assertEquals(new Sword("TestSword2",3,20),
-            testController.getPlayerCharacter(0).getEquippedWeapon());
-
-    testController.getPhase().moveLeftInventory();
-    testController.getPhase().equipWeapon();
+    testController.rightMoveInventory();
+    testController.equipWeapon();
     assertEquals(new Sword("TestSword1",3,20),
+            testController.getPlayerCharacter(0).getEquippedWeapon());
+
+    testController.rightMoveInventory();
+    testController.rightMoveInventory();
+    testController.equipWeapon();
+    assertEquals(new Sword("TestSword3",3,20),
+            testController.getPlayerCharacter(0).getEquippedWeapon());
+
+    testController.upMoveInventory();
+    testController.equipWeapon();
+    assertEquals(new Sword("TestSword0",3,20),
+            testController.getPlayerCharacter(0).getEquippedWeapon());
+
+    testController.downMoveInventory();
+    testController.equipWeapon();
+    assertEquals(new Sword("TestSword4",3,20),
+            testController.getPlayerCharacter(0).getEquippedWeapon());
+
+    testController.leftMoveInventory();
+    testController.equipWeapon();
+    assertEquals(new Sword("TestSword3",3,20),
+            testController.getPlayerCharacter(0).getEquippedWeapon());
+
+    testController.downMoveInventory();
+    testController.downMoveInventory();
+    testController.equipWeapon();
+    assertEquals(new Sword("TestSword9",3,20),
             testController.getPlayerCharacter(0).getEquippedWeapon());
 
   }
