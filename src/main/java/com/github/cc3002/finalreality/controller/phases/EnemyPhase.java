bc8fff75168de540da650485e66eeaa5006c1290
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-12-22 03:42:38 -0300

    Added auto attack.

--- src/main/java/com/github/cc3002/finalreality/controller/phases/EnemyPhase.java
+++ src/main/java/com/github/cc3002/finalreality/controller/phases/EnemyPhase.java
@@ -1,11 +1,16 @@
 package com.github.cc3002.finalreality.controller.phases;
 
-import com.github.cc3002.finalreality.controller.phases.IPhase;
 import com.github.cc3002.finalreality.model.character.Enemy;
 import com.github.cc3002.finalreality.model.character.player.IPlayerCharacter;
 
+/**
+ * The enemy phase, this phase is used to do the enemy automatic attack.
+ */
 public class EnemyPhase extends AbstractPhase {
 
+  /**
+   * Executes this phase, the character attacks a random player character.
+   */
   public void doPhase( ) {
     Enemy actualCharacter = (Enemy) controller.getCharacter();
     int targetIndex = controller.getRandom().nextInt(controller.getPlayersQuantity());
